a20966f44931b2e6eefa9a953d2dfad0f8c614ce
qortjddbs(notebook) <beak980513@naver.com>
2026-09-08 11:40:49 +0900

    2장 끝. 2-1 연습문제까지

---
+++ Server/InitSocket.cpp
@@ -0,0 +1,30 @@
+﻿#define LOBYTE(w) ((BYTE)(w))
+#define HIBYTE(w) ((BYTE)((WORD)(w) >> 8) & 0xFF))
+
+#include "Common.h"
+
+int main(int argc, char *argv[])
+{
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(1, 1), &wsa) != 0)
+		return 1;
+	printf("[알림] 윈속 초기화 성공\n");
+
+	printf("wVersion       : %d.%d\n", LOBYTE(wsa.wVersion), HIBYTE(wsa.wVersion));
+	printf("wHighVersion   : %d.%d\n", LOBYTE(wsa.wHighVersion), HIBYTE(wsa.wHighVersion));
+	printf("szDescription  : %s\n", wsa.szDescription);
+	printf("szSystemStatus : %s\n", wsa.szSystemStatus);
+
+	// 소켓 생성
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+	printf("[알림] 소켓 생성 성공\n");
+
+	// 소켓 닫기
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
